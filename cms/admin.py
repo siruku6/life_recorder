7482f8c80759715e99f6070bcb55741d151aed7a
Seiya Sasaki <sirukufarios@gmail.com>
2021-01-31 23:20:53 +0900

    added tables records, activitiy_types, activities

---
+++ cms/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from cms.models import Record, ActivityType, Activity
+
+admin.site.register(Record)
+admin.site.register(ActivityType)
+admin.site.register(Activity)
---
+++ cms/models.py
@@ -0,0 +1,36 @@
+import datetime
+from django.db import models
+
+class Record(models.Model):
+    """Record"""
+    date = models.DateTimeField(
+        '日付', max_length=255,
+        default=datetime.datetime.today().replace(hour=0, minute=0, second=0, microsecond=0),
+        null=False
+    )
+    comment = models.TextField('コメント', blank=True, null=True)
+    # page = models.IntegerField('ページ数', blank=True, default=0)
+
+    def __str__(self):
+        return self.date.isoformat()
+
+class ActivityType(models.Model):
+    """ActivityType"""
+    name = models.CharField('種別', max_length=50, null=False)
+    color = models.CharField('色コード', max_length=20, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Activity(models.Model):
+    """Activity"""
+    record = models.ForeignKey(Record, verbose_name='活動日', related_name='activities', on_delete=models.CASCADE)
+    activity_type = models.ForeignKey(ActivityType, verbose_name='種別', related_name='activities', on_delete=models.SET_NULL, null=True)
+    name = models.CharField('内容', max_length=255, null=False)
+    start = models.DateTimeField('開始', default=datetime.datetime.now(), null=False)
+    end = models.DateTimeField('終了', default=datetime.datetime.now(), null=False)
+
+    def __str__(self):
+        act_type = self.activity_type
+        display = '{}: {}'.format(self.activity_type.name, self.name) if act_type is not None else self.name
+        return display
