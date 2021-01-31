1866a0084f5616286a5ba640bca8042a2518d587
Seiya Sasaki <sirukufarios@gmail.com>
2021-01-31 23:35:42 +0900

    added columns displayed on the admin view

--- cms/admin.py
+++ cms/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from cms.models import Record, ActivityType, Activity
 
+
+class ActivityAdmin(admin.ModelAdmin):
+    list_display = ('id', 'start', 'end', 'activity_type', 'name')
+    list_display_links = ('id', 'start', 'end', 'activity_type', 'name')
+
+
 admin.site.register(Record)
 admin.site.register(ActivityType)
-admin.site.register(Activity)
+admin.site.register(Activity, ActivityAdmin)
