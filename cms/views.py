42f875a5b9212de4e193b8fcd94c700e87cb02a4
Seiya Sasaki <sirukufarios@gmail.com>
2021-02-21 20:23:43 +0900

    added records list to cms

--- cms/views.py
+++ cms/views.py
@@ -1,10 +1,12 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 from django.http import HttpResponse
+from cms.models import Record  # , ActivityType, Activity
 
 
 def life_logs(request):
     """記録一覧"""
-    return HttpResponse('記録一覧')
+    records = Record.objects.all().order_by('id')
+    return render(request, 'cms/logs.html', {'records': records})
 
 
 def edit_record(request, record_id=None):
