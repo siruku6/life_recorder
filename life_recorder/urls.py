d361503192300becb652b5dd2c6f927f76c9e916
Seiya Sasaki <sirukufarios@gmail.com>
2021-02-25 19:43:29 +0900

    introduced gunicorn

--- life_recorder/urls.py
+++ life_recorder/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
 
 
@@ -21,3 +22,4 @@ urlpatterns = [
     path('cms/', include('cms.urls')),
     path('admin/', admin.site.urls),
 ]
+urlpatterns += staticfiles_urlpatterns()
