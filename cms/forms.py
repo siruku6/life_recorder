343b6c073f1093f6f394a29db1bc13c8a7f9ead7
Seiya Sasaki <sirukufarios@gmail.com>
2021-03-06 16:54:31 +0900

    enabled to create/edit/destroy activity

--- cms/forms.py
+++ cms/forms.py
@@ -1,10 +1,10 @@
 from django.forms import ModelForm
-from bootstrap_datepicker_plus import DatePickerInput  # , TimePickerInput, DateTimePickerInput
-from cms.models import Record
+from bootstrap_datepicker_plus import DatePickerInput, TimePickerInput  # , DateTimePickerInput
+from cms.models import Record, Activity
 
 
 class RecordForm(ModelForm):
-    """活動記録のフォーム"""
+    """活動日のフォーム"""
     class Meta:
         model = Record
         fields = ('date', 'comment', )
@@ -15,5 +15,34 @@ class RecordForm(ModelForm):
                 #     'locale': 'ja',
                 #     'dayViewHeaderFormat': 'YYYY年 MMMM',
                 # }
+            ),
+        }
+
+
+class ActivityForm(ModelForm):
+    """活動内容のフォーム"""
+    # TODO: activity_type は、選択式のみでなく、入力も可能にする
+    #   https://stackoverflow.com/questions/24783275/django-form-with-choices-but-also-with-freetext-option
+    #   https://teratail.com/questions/201160
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     for field in self.fields.values():
+    #         field.widget.attrs['class'] = 'form-control'
+
+    class Meta():
+        model = Activity
+        fields = ('activity_type', 'name', 'start', 'end', )
+        widgets = {
+            'start': TimePickerInput(
+                options={
+                    "format": 'HH:mm',
+                    "stepping": 15,
+                }
+            ),
+            'end': TimePickerInput(
+                options={
+                    "format": 'HH:mm',
+                    "stepping": 15,
+                }
             ),
         }
--- cms/views.py
+++ cms/views.py
@@ -1,10 +1,14 @@
+import datetime
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic.list import ListView
 
-from cms.models import Record  # , ActivityType, Activity
-from cms.forms import RecordForm
+from cms.models import Record, Activity  # , ActivityType
+from cms.forms import RecordForm, ActivityForm
 
 
+# -----------------------------------------------------------
+#                          Record
+# -----------------------------------------------------------
 def life_logs(request):
     """活動日一覧"""
     records = Record.objects.all().order_by('id')
@@ -38,6 +42,9 @@ def del_record(request, record_id):
     return redirect('cms:life_logs')
 
 
+# -----------------------------------------------------------
+#                         Activity
+# -----------------------------------------------------------
 class Activities(ListView):
     """活動内容の一覧"""
     context_object_name = 'activities'
@@ -51,3 +58,44 @@ class Activities(ListView):
 
         context = self.get_context_data(object_list=self.object_list, record=record)
         return self.render_to_response(context)
+
+
+def edit_activity(request, record_id, activity_id=None):
+    """活動内容の編集"""
+    record = get_object_or_404(Record, pk=record_id)
+    if activity_id:
+        activity = get_object_or_404(Activity, pk=activity_id)
+    else:
+        activity = Activity()
+
+    if request.method == 'POST':
+        params = preprocess_activity_params(record=record, params=request.POST.copy())
+        form = ActivityForm(params, instance=activity)
+        if form.is_valid():
+            activity = form.save(commit=False)
+            activity.record = record
+            activity.save()
+            return redirect('cms:activities', record_id=record_id)
+    else:
+        form = ActivityForm(instance=activity)
+
+    return render(
+        request, 'cms/edit_activity.html',
+        dict(form=form, record_id=record_id, activity_id=activity_id))
+
+
+def preprocess_activity_params(record, params):
+    target_date = record.date.replace(tzinfo=datetime.timezone(datetime.timedelta(hours=9)))
+    start_hm = params.get('start').split(':')
+    end_hm = params.get('end').split(':')
+
+    params['start'] = target_date.replace(hour=int(start_hm[0]), minute=int(start_hm[1]))
+    params['end'] = target_date.replace(hour=int(end_hm[0]), minute=int(end_hm[1]))
+    return params
+
+
+def del_activity(request, record_id, activity_id=None):
+    """活動内容の削除"""
+    activity = get_object_or_404(Activity, pk=activity_id)
+    activity.delete()
+    return redirect('cms:activities', record_id=record_id)
