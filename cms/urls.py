7bf628e05301e0a1a9c29dcad74651f091f274d7
Seiya Sasaki <sirukufarios@gmail.com>
2021-03-06 13:56:47 +0900

    enabled to display activities

--- cms/urls.py
+++ cms/urls.py
@@ -3,9 +3,12 @@ from cms import views
 
 app_name = 'cms'
 urlpatterns = [
-    # 書籍
-    path('logs/', views.life_logs, name='life_logs'),   # 一覧
-    path('records/create/', views.edit_record, name='create_record'),  # 登録
-    path('records/edit/<int:record_id>/', views.edit_record, name='edit_record'),  # 修正
-    path('records/destroy/<int:record_id>/', views.del_record, name='destroy_record'),   # 削除
+    # 活動日
+    path('logs/', views.life_logs, name='life_logs'),
+    path('records/create/', views.edit_record, name='create_record'),
+    path('records/edit/<int:record_id>/', views.edit_record, name='edit_record'),
+    path('records/destroy/<int:record_id>/', views.del_record, name='destroy_record'),
+
+    # 活動内容
+    path('records/<int:record_id>/activities/', views.Activities.as_view(), name='activities'),
 ]
--- cms/views.py
+++ cms/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic.list import ListView
 
 from cms.models import Record  # , ActivityType, Activity
 from cms.forms import RecordForm
 
 
 def life_logs(request):
-    """記録一覧"""
+    """活動日一覧"""
     records = Record.objects.all().order_by('id')
     return render(request, 'cms/logs.html', {'records': records})
 
 
 def edit_record(request, record_id=None):
-    """活動記録の編集"""
+    """活動日の編集"""
     if record_id:
         record = get_object_or_404(Record, pk=record_id)
     else:
@@ -30,8 +31,23 @@ def edit_record(request, record_id=None):
 
 
 def del_record(request, record_id):
-    """活動記録の削除"""
+    """活動日の削除"""
     # return HttpResponse('活動記録の削除')
     record = get_object_or_404(Record, pk=record_id)
     record.delete()
     return redirect('cms:life_logs')
+
+
+class Activities(ListView):
+    """活動内容の一覧"""
+    context_object_name = 'activities'
+    template_name = 'cms/activities.html'
+    paginate_by = 2
+
+    def get(self, request, *args, **kwargs):
+        record = get_object_or_404(Record, pk=kwargs['record_id'])
+        activities = record.activities.all().order_by('id')
+        self.object_list = activities
+
+        context = self.get_context_data(object_list=self.object_list, record=record)
+        return self.render_to_response(context)
