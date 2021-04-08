e38e1da0790af0e717e0e51574b7cb9f3a562d48
Seiya Sasaki <sirukufarios@gmail.com>
2021-04-08 09:03:53 +0900

    enabled to run 'docker-compose up'

---
+++ docker/web/cmd.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# wget https://files.pythonhosted.org/packages/70/94/784178ca5dd892a98f113cdd923372024dc04b8d40abe77ca76b5fb90ca6/pytz-2021.1-py2.py3-none-any.whl
+# wget https://files.pythonhosted.org/packages/b8/6f/9a4415cc4fe9228e26ea53cf2005961799b2abb8da0411e519fdb74754fa/Django-3.1.7-py3-none-any.whl#sha256=baf099db36ad31f970775d0be5587cc58a6256a6771a44eb795b554d45f211b8
+# pip install pytz-2021.1-py2.py3-none-any.whl
+# pip install Django-3.1.7-py3-none-any.whl
+
+pip install -r requirements.txt
+# pip install django==3.1.7
