5c3289efe59b1eba2c9ef76669f718135a758261
Seiya Sasaki <sirukufarios@gmail.com>
2021-04-17 12:41:37 +0900

    enable flake8 on circleci

--- cms/models.py
+++ cms/models.py
@@ -14,7 +14,7 @@ class Record(models.Model):
     # page = models.IntegerField('ページ数', blank=True, default=0)
 
     def __str__(self):
-        return self.date.isoformat()
+        return 'id({}) {}'.format(self.id, self.date.isoformat())
 
 
 class ActivityType(models.Model):
@@ -23,18 +23,21 @@ class ActivityType(models.Model):
     color = models.CharField('色コード', max_length=20, blank=True, null=True)
 
     def __str__(self):
-        return self.name
+        return 'id:({}) {}'.format(self.id, self.name)
 
 
 class Activity(models.Model):
     """Activity"""
     record = models.ForeignKey(Record, verbose_name='活動日', related_name='activities', on_delete=models.CASCADE)
-    activity_type = models.ForeignKey(ActivityType, verbose_name='種別', related_name='activities', on_delete=models.SET_NULL, null=True)
+    activity_type = models.ForeignKey(
+        ActivityType, verbose_name='種別', related_name='activities', on_delete=models.SET_NULL, null=True
+    )
     name = models.CharField('内容', max_length=255, null=False)
     start = models.DateTimeField('開始', default=timezone.now, null=False)
     end = models.DateTimeField('終了', default=timezone.now, null=False)
 
     def __str__(self):
         act_type = self.activity_type
-        display = '{}: {}'.format(self.activity_type.name, self.name) if act_type is not None else self.name
+        display = 'id({}) {} - {}'.format(self.id, self.activity_type.name, self.name) \
+            if act_type is not None else self.name
         return display
