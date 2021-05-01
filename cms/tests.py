0b4be1aadeb73a822dc0bb61246cb2a6023f2d04
Seiya Sasaki <sirukufarios@gmail.com>
2021-05-01 15:45:43 +0900

    [Unittest] added one test for view

--- cms/tests.py
+++ cms/tests.py
@@ -1,9 +1,11 @@
+import datetime
+
 from django.test import TestCase
 from django.urls import resolve, Resolver404, reverse
 from django.utils import timezone
 import pytest
 
-from .models import Record
+from .models import Activity, Record
 
 
 class HomePageTest(TestCase):
@@ -27,11 +29,32 @@ class ActivityViewTests(TestCase):
         """
         record = create_record()
         response = self.client.get(reverse('cms:activities', kwargs={'record_id': record.id}))
+
         assert response.status_code == 200
         self.assertContains(response, "No activities are available.")
         assert response.context['activities'].exists() is False
+
+    def test_with_activities(self):
+        """
+        [Example] If some activities exist, those records are displayed.
+        """
+        record = create_record()
+        activity1 = create_activity(record.id, name='act1')
+        activity2 = create_activity(record.id, name='act2')
+        response = self.client.get(reverse('cms:activities', kwargs={'record_id': record.id}))
+
+        assert response.status_code == 200
+        self.assertContains(response, activity1.name)
+        self.assertContains(response, activity2.name)
+        self.assertNotContains(response, "No activities are available.")
 
 
 def create_record(date=None, comment=None):
     if date is None: date = timezone.now()
     return Record.objects.create(date=date, comment=comment)
+
+
+def create_activity(record_id, name=None):
+    start = timezone.now()
+    end = timezone.now() + datetime.timedelta(hours=1)
+    return Activity.objects.create(record_id=record_id, name=name, start=start, end=end)
