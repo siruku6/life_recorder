65126684d3572f3ccfbb1577e900b0bfe4d0933b
Seiya Sasaki <sirukufarios@gmail.com>
2021-05-03 16:36:34 +0900

    [Api] introduced rest_framework for api

---
+++ api/urls.py
@@ -0,0 +1,13 @@
+from django.urls import include, path
+from rest_framework import routers
+from api import views
+
+router = routers.DefaultRouter()
+router.register('users', views.UserViewSet)
+router.register('groups', views.GroupViewSet)
+
+app_name = 'api/v1'
+urlpatterns = [
+    path('', include(router.urls)),
+    # path('v1/records', views.records, name='records')
+]
--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('api/v1/', include('api.urls')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+
     path('cms/', include('cms.urls')),
     path('admin/', admin.site.urls),
 ]
