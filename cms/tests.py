63de7ae0d043d53fd7f04de22bacf17a617d19c2
Seiya Sasaki <sirukufarios@gmail.com>
2021-05-09 16:33:14 +0900

    [DB] added TemplateActivity and added method to create it

--- cms/tests.py
+++ cms/tests.py
@@ -8,7 +8,8 @@ from django.utils import timezone
 from faker import Faker
 import pytest
 
-from cms.models import Activity, ActivityType, Record
+from cms.models import Activity, ActivityType, Record, TemplateActivity
+from cms.views import create_template_activity
 
 
 FAKER_INSTANCE = Faker(['en_US', 'ja_JP'])
@@ -33,8 +34,8 @@ class ActivityModelTests(TestCase):
         """
         [Example] attribute 'spent_time' is always automatically set
         """
-        record = create_record()
-        activity = create_activity(record.id, name='act1')
+        record = factory_create_record()
+        activity = factory_create_activity(record.id, name='act1')
         assert activity.spent_time == 3600
 
 
@@ -44,7 +45,7 @@ class ActivityTypeModelTests(TestCase):
         [Example] attribute 'name' is duplicated
         """
         name = 'test_activity_type'
-        _ = create_activity_type(name=name)
+        _ = factory_create_activity_type(name=name)
         with self.assertRaises(ValidationError):
             duplicated_act_type = ActivityType(name=name)
             duplicated_act_type.full_clean()
@@ -56,18 +57,19 @@ class RecordModelTests(TestCase):
         [Example] attribute 'date' is duplicated
         """
         date = datetime.date.today()
-        _ = create_record(date=date)
+        _ = factory_create_record(date=date)
         with self.assertRaises(ValidationError):
             duplicated_record = Record(date=date)
             duplicated_record.full_clean()
 
 
 class ActivityViewTests(TestCase):
+    """ ActivityView """
     def test_no_activities(self):
         """
         [Example] If no activities exist, an appropriate message is displayed.
         """
-        record = create_record()
+        record = factory_create_record()
         response = self.client.get(reverse('cms:activities', kwargs={'record_id': record.id}))
 
         assert response.status_code == 200
@@ -78,9 +80,9 @@ class ActivityViewTests(TestCase):
         """
         [Example] If some activities exist, those records are displayed.
         """
-        record = create_record()
-        activity1 = create_activity(record.id, name='act1')
-        activity2 = create_activity(record.id, name='act2')
+        record = factory_create_record()
+        activity1 = factory_create_activity(record.id, name='act1')
+        activity2 = factory_create_activity(record.id, name='act2')
         spent_time1 = (activity1.end - activity1.start).seconds / 3600.0
         response = self.client.get(reverse('cms:activities', kwargs={'record_id': record.id}))
 
@@ -91,26 +93,57 @@ class ActivityViewTests(TestCase):
         self.assertContains(response, spent_time1)
         self.assertNotContains(response, "No activities are available.")
 
+    def test_create_template_activity_once(self):
+        """
+        [Example] create a template_activity.
+        """
+        act_type = factory_create_activity_type()
+        tmp_act = create_template_activity({
+            'name': FAKER_INSTANCE.text(max_nb_chars=255),
+            'activity_type': act_type.id
+        })
+        assert TemplateActivity.objects.first() == tmp_act
+
+    def test_update_template_activity(self):
+        """
+        [Example] update activity_type_id of a template_activity.
+        """
+        act_type1 = factory_create_activity_type()
+        act_type2 = factory_create_activity_type()
+        tmp_act_name = FAKER_INSTANCE.sentence()
+
+        tmp_act1 = create_template_activity({
+            'name': tmp_act_name,
+            'activity_type': act_type1.id
+        })
+        tmp_act2 = create_template_activity({
+            'name': tmp_act_name,
+            'activity_type': act_type2.id
+        })
+        first_temp_act = TemplateActivity.objects.first()
+        assert first_temp_act.activity_type_id != tmp_act1.activity_type_id
+        assert first_temp_act.activity_type_id == tmp_act2.activity_type_id
+
 
 class RecordViewTests(TestCase):
     def test_logs(self):
-        record = create_record()
+        record = factory_create_record()
         response = self.client.get(reverse('cms:life_logs'))
         self.assertQuerysetEqual(response.context['records'], [record], ordered=False)
 
 
-def create_record(date=None, comment=None):
+def factory_create_record(date: datetime.date = None, comment: str = None) -> Record:
     if date is None: date = FAKER_INSTANCE.date_time().date()
     if comment is None: comment = FAKER_INSTANCE.text(max_nb_chars=200)
     return Record.objects.create(date=date, comment=comment)
 
 
-def create_activity_type(name=None):
+def factory_create_activity_type(name: str = None) -> ActivityType:
     if name is None: name = 'hoge'
     return ActivityType.objects.create(name=name, color='#123456')
 
 
-def create_activity(record_id, name=None):
+def factory_create_activity(record_id: int, name: str = None) -> Activity:
     now = timezone.now()
     start = now
     end = now + datetime.timedelta(hours=1)
