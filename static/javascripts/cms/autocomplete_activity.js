c9f80491356fde17d28cd4155cfebd144cddc6c2
Seiya Sasaki <sirukufarios@gmail.com>
2021-06-01 08:42:18 +0900

    #48: enabled autocomplete with multiple keywords

--- static/javascripts/cms/autocomplete_activity.js
+++ static/javascripts/cms/autocomplete_activity.js
@@ -1,5 +1,5 @@
 $(function(){
-  const programmingLanguages = [
+  const candidates = [
     'ruby',
     'python',
     'c',
@@ -11,6 +11,35 @@ $(function(){
   ];
 
   $('#activity-name').autocomplete({
-    source: programmingLanguages
+    source:  function(requestObj, responseFunc) {
+      let candidatesArray = candidates.slice();
+      const inputKeywords = requestObj.term.split(/\s+/);
+ 
+      $.each(inputKeywords, function(_, keyword) {
+        let regX = new RegExp(keyword, 'i');
+        candidatesArray = candidatesArray.filter(function(candidate) {
+          return regX.test(candidate);
+        });
+      });
+      
+      return responseFunc(candidatesArray);
+    },
+    open: function(_event, _ui) {
+      // INFO: highlight matched characters in yellow
+      const suggestedList = $("ul.ui-autocomplete > li.ui-menu-item > .ui-menu-item-wrapper");
+      const searchTerm = $.trim($("#activity-name").val()).split(/\s+/).join('|');
+      let regX = new RegExp('(' + searchTerm + ')', "ig");
+
+      suggestedList.each(function() {
+        let $this = $(this);
+        let oldOption = $this.text();
+        $this.html(oldOption.replace(regX, '<span class="autocomplete-custom-highlight">$1</span>'));
+      });
+    },
+    focus: function (_event, _ui) {
+      return false;
+    },
+    // delay: 500,
+    minLength: 2
   });
 });
