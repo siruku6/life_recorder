05a7765dedd22d23337443e3f46d8b62b493d98b
Seiya Sasaki <sirukufarios@gmail.com>
2021-06-01 09:44:32 +0900

    #48: enabled autocomplete in view 'edit_activity' to suggest TemplateActivity

--- static/javascripts/cms/autocomplete_activity.js
+++ static/javascripts/cms/autocomplete_activity.js
@@ -1,45 +1,44 @@
-$(function(){
-  const candidates = [
-    'ruby',
-    'python',
-    'c',
-    'c++',
-    'c#',
-    'php',
-    'go-lang',
-    'Lisp',
-  ];
+let AutocompleteModule = {
+  candidates: $('#autocomplete-candidates > li.candidate')
+    .map(function(_i, candidate) { return candidate.innerText })
+    .get(),
+  filterCandidatesBy: function(inputKeywords) {
+    let candidatesArray = AutocompleteModule.candidates.slice();
+    $.each(inputKeywords, function(_, keyword) {
+      let regX = new RegExp(keyword, 'i');
+      candidatesArray = candidatesArray.filter(function(candidate) {
+        return regX.test(candidate);
+      });
+    });
+    return candidatesArray;
+  },
+  highlightMatchedChars: function() {
+    const suggestedList = $("ul.ui-autocomplete > li.ui-menu-item > .ui-menu-item-wrapper");
+    const searchTerm = $.trim($("#activity-name").val()).split(/\s+/).join('|');
+    let regX = new RegExp('(' + searchTerm + ')', "ig");
 
+    suggestedList.each(function() {
+      let $this = $(this);
+      let oldOption = $this.text();
+      $this.html(oldOption.replace(regX, '<span class="autocomplete-custom-highlight">$1</span>'));
+    });
+  },
+}
+
+$(function() {
   $('#activity-name').autocomplete({
     source:  function(requestObj, responseFunc) {
-      let candidatesArray = candidates.slice();
       const inputKeywords = requestObj.term.split(/\s+/);
- 
-      $.each(inputKeywords, function(_, keyword) {
-        let regX = new RegExp(keyword, 'i');
-        candidatesArray = candidatesArray.filter(function(candidate) {
-          return regX.test(candidate);
-        });
-      });
-      
-      return responseFunc(candidatesArray);
+      let filteredCandidates = AutocompleteModule.filterCandidatesBy(inputKeywords)
+      return responseFunc(filteredCandidates);
     },
     open: function(_event, _ui) {
-      // INFO: highlight matched characters in yellow
-      const suggestedList = $("ul.ui-autocomplete > li.ui-menu-item > .ui-menu-item-wrapper");
-      const searchTerm = $.trim($("#activity-name").val()).split(/\s+/).join('|');
-      let regX = new RegExp('(' + searchTerm + ')', "ig");
-
-      suggestedList.each(function() {
-        let $this = $(this);
-        let oldOption = $this.text();
-        $this.html(oldOption.replace(regX, '<span class="autocomplete-custom-highlight">$1</span>'));
-      });
+      AutocompleteModule.highlightMatchedChars();
     },
     focus: function (_event, _ui) {
       return false;
     },
     // delay: 500,
-    minLength: 2
+    minLength: 1
   });
 });
